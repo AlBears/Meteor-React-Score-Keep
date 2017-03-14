0e1caca8beafb13f5c6f49cf4336d0f9113ea04f
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-14 15:44:40 +0300

    add clients to db

--- server/main.js
+++ server/main.js
@@ -1,1 +1,10 @@
-console.log('hi from the server');
+import { Meteor } from 'meteor/meteor';
+import { Players } from './../imports/api/players';
+
+Meteor.startup(function() {
+  Players.insert({
+    name: "Alex",
+    score: 3
+  });
+  console.log(Players.find().fetch());
+});
