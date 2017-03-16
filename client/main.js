05adcac52f37b89705630bd6bba035ebdee63735
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-16 19:15:51 +0300

    update player

--- client/main.js
+++ client/main.js
@@ -11,7 +11,17 @@ const renderPlayers = (playersList) => {
           <p key={player._id}>
               {player.name} has {player.score} point(s).
               <button onClick={() => {
-                Players.remove({_id: player._id})
+                Players.update(player._id, {$inc: {score:-1}
+                });
+              }}>-1
+              </button>
+              <button onClick={() => {
+                Players.update(player._id, {$inc: {score:1}
+                });
+              }}>+1
+              </button>
+              <button onClick={() => {
+                Players.remove(player._id)
               }}>X</button>
           </p>
         );
