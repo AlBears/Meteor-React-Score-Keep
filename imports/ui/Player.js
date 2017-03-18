19f65c9e119f9391b5363ec3e9b6e68e71909912
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-03-18 13:55:21 +0300

    player component

---
+++ imports/ui/Player.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Players } from '../api/players';
+
+class Player extends React.Component {
+  render() {
+    const { player } = this.props;
+    return (
+          <p key = {player._id}>
+              {player.name} has {player.score} point(s).
+              <button onClick={() => {
+                Players.update(player._id, {$inc: {score:-1}
+                });
+              }}>-1
+              </button>
+              <button onClick={() => {
+                Players.update(player._id, {$inc: {score:1}
+                });
+              }}>+1
+              </button>
+              <button onClick={() => {
+                Players.remove(player._id)
+              }}>X</button>
+          </p>
+        );
+  }
+}
+
+export default Player;
